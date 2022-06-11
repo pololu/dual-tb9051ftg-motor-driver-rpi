b6dc0188483164bcdeab801c70582c6731a5709e
Penumbrage <wwangVer2.0@gmail.com>
2022-06-11 02:28:50 -0600

    modified the setup.py file to work with the single motor modifications

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from distutils.core import setup
-setup(name='dual_tb9051ftg_rpi',
+setup(name='single_tb9051ftg_rpi',
       version='1.0.0',
-      description=('Library for the Pololu Dual TB9051FTG Motor '
+      description=('Library for the Pololu Single TB9051FTG Motor '
                    'Driver for Raspberry Pi'),
-      url='http://www.pololu.com/product/2761',
-      py_modules=['dual_tb9051ftg_rpi'],
+      url='https://www.pololu.com/product/2997',
+      py_modules=['single_tb9051ftg_rpi'],
       )
