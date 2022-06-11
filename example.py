4f7ec77ea9337ae97ba14f19777473dbd6187387
Penumbrage <wwangVer2.0@gmail.com>
2022-06-11 02:27:58 -0600

    modified the example to remove motor 2

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import time
-from dual_tb9051ftg_rpi import motors, MAX_SPEED
+from single_tb9051ftg_rpi import motors, MAX_SPEED
 
 # Define a custom exception to raise if a fault is detected.
 class DriverFault(Exception):
@@ -10,8 +10,8 @@ class DriverFault(Exception):
 def raiseIfFault():
     if motors.motor1.getFault():
         raise DriverFault(1)
-    if motors.motor2.getFault():
-        raise DriverFault(2)
+    # if motors.motor2.getFault():
+    #     raise DriverFault(2)
 
 # Set up sequences of motor speeds.
 test_forward_speeds = list(range(0, MAX_SPEED, 1)) + \
@@ -21,7 +21,7 @@ test_reverse_speeds = list(range(0, -MAX_SPEED, -1)) + \
   [-MAX_SPEED] * 200 + list(range(-MAX_SPEED, 0, 1)) + [0]
 
 try:
-    motors.setSpeeds(0, 0)
+    motors.setSpeeds(0)         # removed the second motor speed
 
     print("Motor 1 forward")
     for s in test_forward_speeds:
@@ -40,17 +40,17 @@ try:
     time.sleep(0.5)
     motors.enable()
 
-    print("Motor 2 forward")
-    for s in test_forward_speeds:
-        motors.motor2.setSpeed(s)
-        raiseIfFault()
-        time.sleep(0.002)
+    # print("Motor 2 forward")
+    # for s in test_forward_speeds:
+    #     motors.motor2.setSpeed(s)
+    #     raiseIfFault()
+    #     time.sleep(0.002)
 
-    print("Motor 2 reverse")
-    for s in test_reverse_speeds:
-        motors.motor2.setSpeed(s)
-        raiseIfFault()
-        time.sleep(0.002)
+    # print("Motor 2 reverse")
+    # for s in test_reverse_speeds:
+    #     motors.motor2.setSpeed(s)
+    #     raiseIfFault()
+    #     time.sleep(0.002)
 
 except DriverFault as e:
     print("Driver %s fault!" % e.driver_num)
